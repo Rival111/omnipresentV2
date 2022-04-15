d6afece5be870d66f890d190552d49f1be025e39
Rival111 <100882307+Rival111@users.noreply.github.com>
2022-04-15 09:33:23 +0700

    Add files via upload

---
+++ assets/preload/data/souls/effectSouls.lua
@@ -0,0 +1,5 @@
+function onCreate()
+	makeLuaSprite('c', 'GreyVG', 0, 0);
+	addLuaSprite('c', false);
+	setObjectCamera('c', 'camHUD');
+end
---
+++ assets/preload/data/souls/opponent.lua
@@ -0,0 +1,13 @@
+function onUpdate(elapsed)
+
+    if bfName == 'bf' or 'bf1' or 'bf-winner' or 'tunnel-bf' then --replace the name for your character name
+        for i=0,4,1 do
+            setPropertyFromGroup('opponentStrums', i, 'texture', 'sex_notes')
+        end
+        for i = 0, getProperty('unspawnNotes.length')-1 do
+            if not getPropertyFromGroup('unspawnNotes', i, 'mustPress') then
+                setPropertyFromGroup('unspawnNotes', i, 'texture', 'sex_notes'); --Change texture
+            end
+        end
+    end
+end
